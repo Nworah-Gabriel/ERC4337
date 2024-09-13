2e039eb886350fea0200737b6fe89358fcf545f0
Nworah Gabriel Chimzuruoke <gabrielnworah6@gmail.com>
2024-09-13 20:31:08 +0000

    Update urls.py

--- wallet_service/urls.py
+++ wallet_service/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from ..wallet_service import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('wallet.urls')),
+    path('',  views.create_account, name='create-account')
 ]
